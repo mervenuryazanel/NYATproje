e02c87dd4032aec333a45877d3e1689108729ee1
merve nur yazanel <54467555+mervenuryazanel@users.noreply.github.com>
2020-05-08 19:36:07 +0300

    Create ISicaklikAlgilayici.java

---
+++ ISicaklikAlgilayici.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nyatproje;
+
+/**
+ *
+ * @author mervenuryazanel B171210069
+ */
+public class ISicaklikAlgilayici {
+    
+}
