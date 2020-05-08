2655105f5c8d098b22ed6641e4e7784a7d267edc
merve nur yazanel <54467555+mervenuryazanel@users.noreply.github.com>
2020-05-08 19:34:18 +0300

    Create IKullanici.java

---
+++ IKullanici.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nyatproje;
+
+/**
+ *
+ * @author mervenuryazanel B171210069
+ */
+public interface IKullanici {
+    String kullaniciAdi="merve";
+    String sifre="0123";
+}
